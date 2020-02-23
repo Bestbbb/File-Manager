6aae8f4c6aa9df5474354f869614f56b2e2d9ef5
Bestbbb <409478176@qq.com>
2020-02-23 23:43:06 +0800

    球求拉

--- main.js
+++ main.js
@@ -40,8 +40,6 @@ function createWindow () {
 }
 
 
-
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
